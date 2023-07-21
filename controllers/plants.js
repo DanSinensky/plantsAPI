fc06ee6ada12d0a68c38f98599794a7c5c938de1
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-07-21 15:34:14 -0400

    updates routes with error messages

--- controllers/plants.js
+++ controllers/plants.js
@@ -55,67 +55,166 @@ export const deletePlant = async (req, res) => {
 };
 
 export const getDroughtTolerant = async (req, res) => {
-  let droughtTolerant = await Plant.find({ "drought_tolerant": req.params.drought_tolerant });
+  try {
+    const droughtTolerant = await Plant.find({ "drought_tolerant": req.params.drought_tolerant.charAt(0).toLowerCase() + req.params.drought_tolerant.slice(1) });
 
-  res.json(droughtTolerant)
+    if (droughtTolerant) {
+      return res.json(droughtTolerant);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getSaltTolerant = async (req, res) => {
-  let saltTolerant = await Plant.find({ "salt_tolerant": req.params.salt_tolerant });
+try {
+    const saltTolerant = await Plant.find({ "salt_tolerant": req.params.salt_tolerant.charAt(0).toLowerCase() + req.params.salt_tolerant.slice(1) });
 
-  res.json(saltTolerant)
+    if (saltTolerant) {
+      return res.json(saltTolerant);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getThorny = async (req, res) => {
-  let thorny = await Plant.find({ "thorny": req.params.thorny });
+  try {
+    const thorny = await Plant.find({ "thorny": req.params.thorny.charAt(0).toLowerCase() + req.params.thorny.slice(1) });
 
-  res.json(thorny)
+    if (thorny) {
+      return res.json(thorny);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getInvasive = async (req, res) => {
-  let invasive = await Plant.find({ "invasive": req.params.invasive });
+  try {
+    const invasive = await Plant.find({ "invasive": req.params.invasive.charAt(0).toLowerCase() + req.params.invasive.slice(1) });
 
-  res.json(invasive)
+    if (invasive) {
+      return res.json(invasive);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getTropical = async (req, res) => {
-  let tropical = await Plant.find({ "tropical": req.params.tropical });
+  try {
+    const tropical = await Plant.find({ "tropical": req.params.tropical.charAt(0).toLowerCase() + req.params.tropical.slice(1) });
 
-  res.json(tropical)
+    if (tropical) {
+      return res.json(tropical);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getIndoor = async (req, res) => {
-  let indoor = await Plant.find({ "indoor": req.params.indoor });
+  try {
+    const indoor = await Plant.find({ "indoor": req.params.indoor.charAt(0).toLowerCase() + req.params.indoor.slice(1) });
 
-  res.json(indoor)
+    if (indoor) {
+      return res.json(indoor);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getLeaf = async (req, res) => {
-  let leaf = await Plant.find({ "leaf": req.params.leaf })
+  try {
+    const leaf = await Plant.find({ "leaf": req.params.leaf.charAt(0).toLowerCase() + req.params.leaf.slice(1) });
 
-  res.json(leaf)
+    if (leaf) {
+      return res.json(leaf);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getLeafColor = async (req, res) => {
-  let leafColor = Plant.find({ leaf_color: req.params.leaf_color })
+  try {
+    const leafColor = Plant.find({ leaf_color: req.params.leaf_color })
 
-  res.json(leafColor)
+    if (leafColor) {
+      return res.json(leafColor)
+    }
+
+    res.status(404).json({ message: "Enter a leaf color"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getEdibleLeaf = async (req, res) => {
-  let edibleLeaf = await Plant.find({ "edible_leaf": req.params.edible_leaf })
+  try {
+    const edibleLeaf = await Plant.find({ "edible_leaf": req.params.edible_leaf.charAt(0).toLowerCase() + req.params.edible_leaf.slice(1) });
 
-  res.json(edibleLeaf)
+    if (edibleLeaf) {
+      return res.json(edibleLeaf);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getNeedsWatering = async (req, res) => {
-  let needsWatering = await Plant.find({ watering: req.params.watering.charAt(0).toUpperCase() + req.params.watering.slice(1)});
+  try {
+    const watering = await Plant.find({ "watering": req.params.watering.charAt(0).toUpperCase() + req.params.watering.slice(1) });
 
-  res.json(needsWatering);
+    if (watering) {
+      return res.json(watering);
+    }
+
+    res.status(404).json({ message: "Enter 'true' or 'false'"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
 
 export const getOrigin = async (req, res) => {
-  let origin = await Plant.find({ origin: req.params.origin.charAt(0).toUpperCase() + req.params.origin.slice(1) });
+ try {
+    const origin = await Plant.find({ origin: req.params.origin.charAt(0).toUpperCase() + req.params.origin.slice(1) });
 
-  res.json(origin);
+    if (origin) {
+      return res.json(origin)
+    }
+
+    res.status(404).json({ message: "Enter a country or region"})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
 };
