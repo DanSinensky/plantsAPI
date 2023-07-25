4a7b47d83e1b54effa5af877a5c889a21d95eb1a
Dan Sinensky <dan.i.sinensky@gmail.com>
2023-07-25 11:47:21 -0400

    removes commented out routes, prunes json data

--- routes/plants.js
+++ routes/plants.js
@@ -9,16 +9,5 @@ router.get("/:id", controllers.getPlant);
 router.post("/", controllers.createPlant);
 router.put("/:id", controllers.updatePlant);
 router.delete("/:id", controllers.deletePlant);
-// router.get("/origin/:origin", controllers.getOrigin);
-// router.get("/watering/:watering", controllers.getNeedsWatering);
-// router.get("/leaf/:leaf", controllers.getLeaf);
-// router.get("/leaf_color/:leaf_color", controllers.getLeafColor);
-// router.get("/edible_leaf/:edible_leaf", controllers.getEdibleLeaf)
-// router.get("/drought_tolerant/:drought_tolerant", controllers.getDroughtTolerant);
-// router.get("/salt_tolerant/:salt_tolerant", controllers.getSaltTolerant);
-// router.get("/thorny/:thorny", controllers.getThorny);
-// router.get("/invasive/:invasive", controllers.getInvasive);
-// router.get("/tropical/:tropical", controllers.getTropical);
-// router.get("/indoor/:indoor", controllers.getIndoor);
 
 export default router;
